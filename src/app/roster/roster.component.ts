29032d388f70932164b7b23124a8f1ef316831a1
Brian Rush <yankeh@gmail.com>
2017-07-14 18:09:32 -0700

    add expanded roster view to the admin route

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -14,6 +14,7 @@ import { FirebaseListObservable } from 'angularfire2/database';
 })
 export class RosterComponent implements OnInit {
   players: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private playerService: PlayerService) { }
 
